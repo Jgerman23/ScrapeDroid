a0daf3b27d044069e090afafc4f64ba2650a9f8c
Justin German <justin.german23@gmail.com>
2019-07-17 18:09:27 -0500

    deleted hndlbrs, fixed server for title link & sum

---
+++ public/app.js
@@ -0,0 +1,49 @@
+$.getJSON("/articles", function (data) {
+    for (var i = 0; i < data.length; i++) {    
+        $(".containers").append("<p>" + data[i].title + "</p>" + "<p id='scrapedLink'>" + data[i].link + "</p>" + "<p id='scrapedSum'>" + data[i].summary + "</p>");
+        $(".containers").append("<button data-id='" + data[i]._id + "' type='button' class='btn btn-info' id='addNotebtn'>Add A Note</button>")
+    }
+});
+
+
+$(document).on("click", "#addNotebtn", function () {    
+    $("#notes").empty();    
+    var thisId = $(this).attr("data-id");
+    
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+    })        
+        .then(function (data) {
+            console.log(data);            
+            $("#notes").append("<h2>" + data.title + "</h2>");            
+            $("#notes").append("<input id='titleinput' name='title' >");            
+            $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");            
+            $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+            
+            if (data.note) {                
+                $("#titleinput").val(data.note.title);                
+                $("#bodyinput").val(data.note.body);
+            }
+        });
+});
+
+$(document).on("click", "#savenote", function () {    
+    var thisId = $(this).attr("data-id");
+    
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + thisId,
+        data: {            
+            title: $("#titleinput").val(),            
+            body: $("#bodyinput").val()
+        }
+    })        
+        .then(function (data) {            
+            console.log(data);            
+            $("#notes").empty();
+        });
+    
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+});
---
+++ server.js
@@ -0,0 +1,88 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 8080;
+
+var app = express();
+
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/ScrapeDroid", { useNewUrlParser: true });
+
+
+app.get("/scrape", function (req, res) {
+    axios.get("https://www.androidpolice.com/").then(function (response) {
+        var $ = cheerio.load(response.data);
+
+        $("div.post").each(function (i, element) {
+            var result = {};
+
+            result.title = $(this).find("header").find("h2").text();
+            result.link = $(this).find("header").find("h2").find("a").attr("href");
+            result.summary = $(this).find("div.post-content").find("p").text();
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+        res.send("Scrape Complete");
+    });
+});
+
+
+app.get("/articles", function (req, res) {    
+    db.Article.find({})
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        })
+});
+
+
+app.get("/articles/:id", function (req, res) {                    
+
+    db.Article.find({ "_id": req.params.id })
+        .populate("note")
+        .then(function (dbArticle) {
+            res.json(dbArticle[0]);
+        })
+        .catch(function (err) {
+            res.json(err);
+        })
+});
+
+
+app.post("/articles/:id", function (req, res) {                    
+
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate({ "_id": req.params.id }, { $set: { note: dbNote._id } }, { new: true });
+        })
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+
+});
+
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
